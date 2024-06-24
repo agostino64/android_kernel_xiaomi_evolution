d8a30201870cd552202e977a7d8ec62cf187631b
Agustin <gutierrezaverruz0@hotmail>
2024-06-24 11:20:30 -0600

    adapt to merge
    Signed-off-by: Agustin <gutierrezaverruz0@hotmail>

--- drivers/power/supply/mediatek/charger/mtk_pd_adapter.c
+++ drivers/power/supply/mediatek/charger/mtk_pd_adapter.c
@@ -463,7 +463,7 @@ static int pd_get_cap(struct adapter_device *dev,
 
 	uint8_t cap_i = 0;
 	int ret;
-	unsigned int idx = 0;
+	int idx = 0;
 	int i;
 	struct mtk_pd_adapter_info *info;
 	int timeout = 0;
